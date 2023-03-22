009ef019381de145dbf249c6e73f41d76d3f991d
liew <127520432+liewchooichin@users.noreply.github.com>
2023-03-22 21:06:08 +0800

    Use fancy indexing to choose random index to partition the dataset

--- ch2_fancy_index2.py
+++ ch2_fancy_index2.py
@@ -36,3 +36,18 @@ if __name__ == "__main__":
     """ first, choose 20 random indices with no repeats, """
     """ and use these indices to select a portion """
     """ of the original array. """
+    """ first, make the 2D array a 1D array; second, choose 20 random points from the newly made 1D array. Random choice: no repeat. """
+    print("Choose 20 random points from the array.")
+    points_index = np.random.choice(arr_normal.shape[0], 20, replace=False)
+    print(f'{points_index = }')
+
+    """ After the points are chosen, we use fancy indexing to index the 2D array. """
+    selected_array = arr_normal[points_index]
+    print("Check that the selected array is in the same dimensions as the original array.")
+    print(f'Shape of {selected_array.shape = }')
+
+    print("Now to see which points were selected, let’s over-plot large circles at the locations of the selected points.")
+    plt.scatter(arr_normal[:, 0], arr_normal[:, 1], alpha=0.3)
+    plt.scatter(selected_array[:, 0], selected_array[:, 1], facecolor='none', s=200);
+
+    print("This kind of strategy is often used to quickly partition datasets, as is often needed in train/test splitting for validation of statistical models.")
